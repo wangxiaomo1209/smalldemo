0d15f7c995b277787b0b466e3c8103ee200c369c
feiyaweb002 <feiyaweb002@feiya.local>
2019-07-03 11:51:20 +0800

    fix problem

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <router-link
-      tag="div"
-      to="/"
-      class="header-abs"
-      v-show="showAbs"
-    >
-      <div class="iconfont header-abs-back">&#xe624;</div>
-    </router-link>
-    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+    <div class="header-fixed">
       <router-link to="/">
         <div class="iconfont header-fixed-back">&#xe624;</div>
       </router-link>
@@ -19,49 +11,12 @@
 
 <script>
 export default {
-  name: 'DetailHeader',
-  data () {
-    return {
-      showAbs: true,
-      opacityStyle: {
-        opacity: 0
-      }
-    }
-  },
-  methods: {
-    handleScroll () {
-      const top = document.documentElement.scrollTop
-      if (top > 60) {
-        let opacity = top / 140
-        opacity = opacity > 1 ? 1 : opacity
-        this.opacityStyle = { opacity }
-        this.showAbs = false
-      } else {
-        this.showAbs = true
-      }
-    }
-  },
-  mounted () {
-    window.addEventListener('scroll', this.handleScroll)
-  }
+  name: 'DetailHeader'
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
-  .header-abs
-    position: absolute
-    left: .2rem
-    top: .2rem
-    width: .8rem
-    height: .8rem
-    line-height: .8rem
-    border-radius: .4rem
-    text-align: center
-    background: rgba(0, 0, 0, .8)
-    .header-abs-back
-      color: #fff
-      font-size: .4rem
   .header-fixed
     position: fixed
     top: 0
@@ -73,6 +28,7 @@ export default {
     color: #fff
     background: $bgColor
     font-size: .32rem
+    opacity: .8
     .header-fixed-back
       position: absolute
       top: 0
